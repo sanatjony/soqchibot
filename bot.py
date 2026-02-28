71b938c3e6192041948bed38dbec2876368d2a51
Sanatjony <50242336+sanatjony@users.noreply.github.com>
2026-03-01 04:58:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,25 +114,30 @@ async def mute_user(message: types.Message):
     user_id = message.reply_to_message.from_user.id
 
     try:
-        await bot.restrict_chat_member(
-            message.chat.id,
-            user_id,
-            ChatPermissions(
-                can_send_messages=False,
-                can_send_audios=False,
-                can_send_documents=False,
-                can_send_photos=False,
-                can_send_videos=False,
-                can_send_video_notes=False,
-                can_send_voice_notes=False,
-                can_send_polls=False,
-                can_send_other_messages=False,
-                can_add_web_page_previews=False
-           ),
-           until_date=until_date
-       )
-    except:
-       return
+    await bot.restrict_chat_member(
+        chat_id=message.chat.id,
+        user_id=user_id,
+        permissions=ChatPermissions(
+            can_send_messages=False,
+            can_send_audios=False,
+            can_send_documents=False,
+            can_send_photos=False,
+            can_send_videos=False,
+            can_send_video_notes=False,
+            can_send_voice_notes=False,
+            can_send_polls=False,
+            can_send_other_messages=False,
+            can_add_web_page_previews=False,
+            can_change_info=False,
+            can_invite_users=False,
+            can_pin_messages=False
+        ),
+        use_independent_chat_permissions=True,
+        until_date=until_date
+    )
+except Exception as e:
+    print("Mute error:", e)
+    return
 
     asyncio.create_task(auto_delete(message))
 
@@ -253,6 +258,7 @@ async def main():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+
 
 
 
