59909b6f3dd9e26e09430f4426412566cc6570df
Sanatjony <50242336+sanatjony@users.noreply.github.com>
2026-03-01 05:11:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,24 +99,12 @@ async def mute_user(message: types.Message):
     user_id = message.reply_to_message.from_user.id
 
     try:
-        # TO'LIQ BLOK
+        # MUHIM: global chatga mute
         await bot.restrict_chat_member(
             chat_id=message.chat.id,
             user_id=user_id,
             permissions=ChatPermissions(
-                can_send_messages=False,
-                can_send_audios=False,
-                can_send_documents=False,
-                can_send_photos=False,
-                can_send_videos=False,
-                can_send_video_notes=False,
-                can_send_voice_notes=False,
-                can_send_polls=False,
-                can_send_other_messages=False,
-                can_add_web_page_previews=False,
-                can_invite_users=False,
-                can_pin_messages=False,
-                can_change_info=False
+                can_send_messages=False
             ),
             use_independent_chat_permissions=True,
             until_date=until_date
@@ -224,5 +212,6 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
